6b01715b8956e4d1ca84e344f007d4bb95be7344
Roger Camargo <roger.camargo@buser.com.br>
2022-08-08 11:27:26 -0300

    Adiciona exercício 2

---
+++ lista-01/js02-metros-para-centimetros.js
@@ -0,0 +1,25 @@
+/**
+ * Faça um programa que converta metros para centímetros
+ * 
+ * ENTRADA         SAÍDA
+ *    1              100
+ *  100            10000
+ *   .5              500
+ * 
+ * Tarefa:
+ * - Altere a variável cm para calcular o valor com base na entrada (metros)
+ *   $ node lista-01/js02-metros-para-centimetros.js
+ *   👉 1 metro(s) == 100 centímetro(s)
+ *  - altere a variável de entrada metros para os valores da lista de ENTRADA/SAÍDA
+ *    e compare se o resultado é o esperado
+ */
+
+// Entrada
+var metros = 1
+
+// Altere a expressão abaixo para calcular cm com base na var metros
+var cm = 100
+
+var resultado = '👉 ' + metros + ' metro(s) == ' + cm + ' centímetro(s)'
+
+console.log(resultado)
