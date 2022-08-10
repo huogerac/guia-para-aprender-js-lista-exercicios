44ef73d0f24751ba3726308046b9e9271bb74b14
Roger Camargo <roger.camargo@buser.com.br>
2022-08-10 11:33:08 -0300

    Adiciona exercício 7

---
+++ lista-01/js07-string-2-boolean.js
@@ -0,0 +1,20 @@
+/**
+ * Faça um programa que retorne true ou false conforme a entra Y,y,yes,S,sim 
+ * 
+ * ENTRADA          SAÍDA
+ * Y                         true
+ * y                         true
+ * S                         true
+ * s                         true
+ * 1                         true
+ * n,N,J,0, espaço           false
+ * qualquer outra coisa, $   false
+ */
+
+// Entrada
+var entrada = 'J'
+
+// implemente aqui o condicional para retornar o boolean (true/false) com base na entrada
+var resultado = false
+
+console.log(resultado)
