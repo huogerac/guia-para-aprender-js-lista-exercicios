0ba90bb4723a77483366c7a4a1f6dfe99bc79364
Roger Camargo <roger.camargo@buser.com.br>
2022-08-10 11:16:52 -0300

    Adiciona mais exercícios

---
+++ lista-01/js04-soma.js
@@ -0,0 +1,20 @@
+/**
+ * Faça um Programa que imprima a soma de dois numeros (inteiros ou com casas decimais).
+ * Se os valores de entrada não forem números válidos, imprima uma mensagem de aviso.
+ * 
+ * ENTRADA                           SAÍDA
+ *    10 e 20                        20
+ *    40.5 e 1.5                     42
+ *    40,5 e 1,5                     42
+ *    A e 40                         'A' não é um número
+ *    40 e A                         'A' não é um número
+ */
+
+// Entrada
+var n1 = 'A'
+var n2 = 40
+
+// Saída
+var resultado = '?'
+
+console.log(resultado)
---
+++ lista-01/js05-celcius.js
@@ -0,0 +1,24 @@
+/**
+ * Faça um Programa que peça a temperatura em Fahrenheit, 
+ * transforme e mostre graus Celsius. 
+ * 
+ * ENTRADA         SAÍDA
+ *    1              100
+ *  100            10000
+ *   .5              500
+ * 
+ * Tarefa:
+ * - Altere a variável cm para calcular o valor com base na entrada (metros)
+ *   $ node lista-01/js02-metros-para-centimetros.js
+ *   👉 1 metro(s) == 100 centímetro(s)
+ *  - altere a variável de entrada metros para os valores da lista de ENTRADA/SAÍDA
+ *    e compare se o resultado é o esperado
+ */
+
+// Entrada
+var fahrenheit = 76
+
+// Altere a expressão abaixo para converter Fahrenheit em Celcius
+var celcius = 32
+
+console.log(celcius)
