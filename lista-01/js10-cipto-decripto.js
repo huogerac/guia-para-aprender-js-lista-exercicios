b68302242aac819f089de45f02402b99371f6eaa
Roger Camargo <roger.camargo@buser.com.br>
2022-08-10 15:43:33 -0300

    Adiciona ex 10

---
+++ lista-01/js10-cipto-decripto.js
@@ -0,0 +1,29 @@
+/**
+ * Faça um programa que retorne true ou false conforme a entrada Y,y,yes,S,sim 
+ * 
+ * ENTRADA          SAÍDA
+ * abacate          bcbduf
+ * zebra            @fcsb
+ * Zebra            $fcsb
+ * 
+ * 
+ * 
+ * DICAS:
+ * É uma variante do exercício js06
+ * Pesquise a tabela ASCII, ai vamos ver que a = 97, que A = 65
+ * (cada caracter no teclado tem uma representação numérica)
+ * Pesquise os métodos de String fromCharCode que converte a para 97
+ * e charCodeAt que faz 97 voltar para a
+ * Assim, no podemos usar um método bem simples de criptografia que seria mudar
+ * o código ASCII de forma a incrementar 1, ou seja, mudar de a para b, b para c 
+ * e assim por diante!
+ * quando for z ou Z mudar para @ e $ respectivamente ao invés de {
+ */
+
+// Entrada
+var entrada = 'J'
+
+// implemente aqui o condicional para retornar o boolean (true/false) com base na entrada
+var resultado = false
+
+console.log(resultado)
