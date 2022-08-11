a3f79d3d564b34080087f812eae1c0bcea947d96
Fernanda Liz <fernandaliz2004@hotmail.com>
2022-08-11 15:24:47 -0300

    corrigido erro de typo exercicio 11

--- lista-01/js11-decrypt.js
+++ lista-01/js11-decrypt.js
@@ -1,20 +1,20 @@
 /**
  * Faça um programa que faça o inverso do programa anterior, ou seja,
- * dado uma entrada criptografada, gere o texto decriptografado 
- * 
+ * dado uma entrada criptografada, gere o texto decriptografado
+ *
  * ENTRADA          SAÍDA
- * bcbduf           abacate
+ * bcbdbuf          abacate
  * @fcsb            zebra
  * $fcsb            Zebra
- * 
+ *
  * DICAS:
  * ao invés incrementar, vamos precisar decrementar...
  */
 
 // Entrada
-var texto = 'bcbduf'
+var texto = "bcbdbuf";
 
 // implemente aqui uma lógica para descriptografar o texto
-var textoDecripto = 'abacate'
+var textoDecripto = "abacate";
 
-console.log(textoDecripto)
+console.log(textoDecripto);
