01f287a5b114b6313ff5ea043e154fc00c53d0c0
Roger Camargo <huogerac@gmail.com>
2022-08-10 22:41:36 -0300

    Fix type no js05 + melhor caso de testes. Obrigado @inczDan

---
+++ lista-01/js05-celsius.js
@@ -0,0 +1,17 @@
+/**
+ * Faça um Programa que peça a temperatura em Fahrenheit, 
+ * transforme e mostre graus Celsius. 
+ * 
+ * ENTRADA              SAÍDA
+ * 102                  39
+ * 89                   32
+ * 70                   21
+ */
+
+// Entrada
+var fahrenheit = 89
+
+// Altere a expressão abaixo para converter Fahrenheit em Celsius
+var celsius = 32
+
+console.log(celsius)
