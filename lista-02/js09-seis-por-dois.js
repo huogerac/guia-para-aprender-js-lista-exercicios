c856120ff378038fc6479d9af0081d38bad34c65
Roger Camargo <huogerac@gmail.com>
2022-08-11 22:36:41 -0300

    Adiciona js09 da lista 2

---
+++ lista-02/js09-seis-por-dois.js
@@ -0,0 +1,14 @@
+/**
+ * Nosso amigo Erick trabalha em uma jornada de seis por dois, ou seja,
+ * ele trabalha 6 dias e folga 2 dias.
+ * Problema: Ele nunca sabe se um dia específico no futuro ele vai estar
+ * trabalhando ou de folga. Difícil fazer planos.
+ * Exemplo, como saber no dia 12 de Outubro ele vai estar trabalhando ou de folga?
+ * Como fazer um programa que liste os próximos 3 meses com dias de trabalho/folga?
+ * 
+ */
+
+// Entrada
+// Pŕoxima volta
+
+// Saída com base na próxima folga, calcular dia de trabalho/folga
