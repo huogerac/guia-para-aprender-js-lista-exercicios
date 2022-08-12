c3b868cb9a1476037313644bed739495619d1ca9
Marcela Lima <marcelalima.df@gmail.com>
2022-08-12 11:59:06 -0300

    exercício 04

--- lista-01/js04-soma.js
+++ lista-01/js04-soma.js
@@ -18,8 +18,10 @@
 // Entrada
 var n1 = 'A'
 var n2 = 40
+soma = n1 +n2
+ if (typeof n1 == 'number' && typeof n2 == 'number') {
+    console.log(soma)
+ } else {
+    console.log('uma das entradas não é um número')
+ }
 
-// Saída
-var resultado = '?'
-
-console.log(resultado)
