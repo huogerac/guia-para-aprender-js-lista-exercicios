7943abfb1913b150cf50369cf40fd818b0b6092c
Roger Camargo <huogerac@gmail.com>
2022-08-12 09:46:23 -0300

    fix no abacate do js10

--- lista-01/js10-crypt.js
+++ lista-01/js10-crypt.js
@@ -4,7 +4,7 @@
  * apenas converta a para b, b para c e assim por diante. 
  * 
  * ENTRADA          SAÍDA
- * abacate          bcbduf
+ * abacate          bcbdbuf
  * zebra            @fcsb
  * Zebra            $fcsb
  * 
@@ -14,11 +14,17 @@
  * (cada caracter no teclado tem uma representação numérica)
  * Pesquise os métodos de String fromCharCode que converte a para 97
  * e charCodeAt que faz 97 voltar para a
- * Assim, nos podemos usar um método bem simples de criptografia que seria mudar
+ * Assim, no podemos usar um método bem simples de criptografia que seria mudar
  * o código ASCII de forma a incrementar 1, ou seja, mudar de a para b, b para c 
  * e assim por diante!
  * quando for z ou Z mudar para @ e $ respectivamente ao invés de {
  * 
+ * Tarefa
+ * - entenda como converter a para 97
+ * - percorra o abacate para gerar os ordinais correspondente: 97,  98, 97 ...
+ * - percorra a lista dos inteiros corresponde somando 1       98, 100, 98 ...
+ * - percorra novamente, agora voltando para caractere          b,   c, b  ...
+ *
  * IMPORTANTE:
  * - Não usar bibliotecas, basta manipular a string de entrada de forma a embaralhá-la
  */
@@ -28,6 +34,6 @@ var texto = 'abacate'
 
 // implemente aqui uma lógica para criptografar o texto
 
-var textoCripto = 'bcbduf'
+var textoCripto = 'bcbdbuf'
 
 console.log(textoCripto)
