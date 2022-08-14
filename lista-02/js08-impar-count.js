a543f83cf3a9cd74437d148401f9df9c8087b8fb
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-08-14 15:19:10 -0300

    typos lista-02/js08-impar-count.js

--- lista-02/js08-impar-count.js
+++ lista-02/js08-impar-count.js
@@ -1,9 +1,9 @@
 /**
- * Percorra a entrada com uma lista de nros e retorne qtos numeros impares/pares
- * 
+ * Percorra a entrada com uma lista de números e retorne quantos números impares/pares existem na mesma
+ *
  * ENTRADA:
  * [1,2,3,4,5,6,7]
- * 
+ *
  * SAIDA
  * 4 3
  */
@@ -11,9 +11,9 @@
 // Entrada
 var entrada = [1,2,3,4,5,6,7]
 
-// processamento
+// Processamento
 
-//saida
+// Saída
 var impares = 4,
     pares = 3
 
